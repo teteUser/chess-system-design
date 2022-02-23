76f34320e61f94f019ca61104cb47b387acac97e
Tennison Capra <tennison_capra@hotmail.com>
2022-02-22 22:26:51 -0300

    ChessException and ChessPosition

--- src/main/java/chess/ChessPosition.java
+++ src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import boardgame.Position;
+
 public class ChessPosition {
 
     private char column;
@@ -20,6 +22,19 @@ public class ChessPosition {
 
     public int getRow() {
         return row;
+    }
+
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
     }
 
 }
