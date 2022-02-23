ac3073f52a3a402d37689abc692fbedb1f3d5549
Tennison Capra <tennison_capra@hotmail.com>
2022-02-23 15:31:58 -0300

    moving pieces

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,15 +2,29 @@ package application;
 
 import boardgame.Board;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
 
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(scan);
 
+            System.out.print("\nTarget: ");
+            ChessPosition target = UI.readChessPosition(scan);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
 
     }
 
